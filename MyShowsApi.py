a9e303a5129693dc3f75d852825c09616d37ce7f
texnichnyi <yuriikushpit@gmail.com>
2018-02-06 14:31:49 +0200

    Performed refactor [Add prepareJson method for more simple request logic]

--- MyShowsApi.py
+++ MyShowsApi.py
@@ -19,36 +19,36 @@ def performRequest (jsonValues=None, headers=None):
     request = urllib2.Request(url, json.dumps(jsonValues), headers=headers)
     return urllib2.urlopen(request)
 
-#add function for search show to get id for 'Add TV show by name to watch list' functionality
-def performedSearch(showName):
-    showIds = []
+#add function for preparing json
+def prepareJson(methodName, parameters={}):
     requestJson = {
         "jsonrpc": "2.0",
-        "method": "shows.Search",
-        "params": {
-            "query": showName
-        },
+        "method": methodName,
+        "params": parameters,
         "id": 1
     }
+    return requestJson
+
+#add function for search show to get id for 'Add TV show by name to watch list' functionality - > Request:[shows.Search]
+def performedSearch(showName):
+    showIds = []
+    parameters = {"query": showName}
+    requestJson = prepareJson("shows.Search", parameters)
     response = simplejson.load(performRequest(requestJson, standartHeader))
     for data in response["result"]:
         if data["titleOriginal"] == showName:
             showIds.append (data["id"])
     return showIds
 
-#performed function for 'Add TV show by name to watch list'
+#performed function for 'Add TV show by name to watch list' - > Request:[lists.AddShow]
 def addShowByName(nameOfShow):
     showIds = performedSearch(showName=nameOfShow)
     for showId in showIds:
-        requestJson = {
-        "jsonrpc": "2.0",
-        "method": "lists.AddShow",
-        "params": {
+        parameter = {
             "id": showId,
             "list": "favorites"
-        },
-        "id": 1
-    }
+        }
+        requestJson = prepareJson("lists.AddShow", parameter)
         performRequest(requestJson, standartHeader)
 
 '''
@@ -57,62 +57,40 @@ the logic of this functionality is next:
 1. I decided to use request 'profile.Shows'
 2. To understand if user watch correct show I check if parameter 'watchedEpisodes' equals 0
 if yes then user doesn't watched show.
+Request:[profile.Shows]
 '''
 
-def getUnWachedShows():
+def getUnWatchedShows():
     unWatchedShowList = []
-    requestJson = {
-         "jsonrpc": "2.0",
-         "method": "profile.Shows",
-         "params": {
-
-        },
-        "id": 1
-    }
+    requestJson = prepareJson("profile.Shows")
     response = simplejson.load(performRequest(requestJson, standartHeader))
     for data in response["result"]:
         if data["watchedEpisodes"] == 0:
             unWatchedShowList.append(data["show"]["title"])
     return unWatchedShowList
 
-#performed function for 'Get names of unwatched episodes'
+
+#performed function for 'Get names of unwatched episodes' - > Request:[lists.Episodes]
 def getUnWatchedEpisodesName():
     names = []
-    requestJson = {
-        "jsonrpc": "2.0",
-        "method": "lists.Episodes",
-        "params": {
-            "list": "unwatched"
-        },
-        "id": 1
-    }
+    parameters = {"list": "unwatched"}
+    requestJson = prepareJson("lists.Episodes", parameters)
     response = simplejson.load(performRequest(requestJson, standartHeader))
     for data in response["result"]:
         names.append(data["episode"]["title"])
     return names
 
-#performed function for 'Mark 1 episode as watched by it's id'
-def checkFirstEpisodeAsWatched():
-    requestJson = {
-        "jsonrpc": "2.0",
-        "method": "lists.Episodes",
-        "params": {
-            "list": "unwatched"
-        },
-        "id": 1
-    }
-    response = simplejson.load(performRequest(requestJson, standartHeader))
-    requestChangeById = {
-         "jsonrpc": "2.0",
-         "method": "manage.CheckEpisode",
-         "params": {
-             "id": response["result"][0]["episode"]["id"],
-             "rating": 0
-         },
-        "id": 1
-    }
-    simplejson.load(performRequest(requestChangeById, standartHeader))
 
+#performed function for 'Mark 1 episode as watched by it's id' - > Request:[lists.Episodes] - > Request:[manage.CheckEpisode]
+def checkFirstEpisodeAsWatched():
+    requestJson = prepareJson("lists.Episodes", {"list": "unwatched"})
+    response = simplejson.load(performRequest(requestJson, standartHeader))
+    parameters = {
+        "id": response["result"][0]["episode"]["id"],
+        "rating": 0
+    }
+    requestChangeById = prepareJson("manage.CheckEpisode", parameters)
+    simplejson.load(performRequest(requestChangeById, standartHeader))
 
 '''
 Performed function for 'Mark all episodes in one show with given name as watched'
@@ -120,34 +98,29 @@ the logic of this functionality is next:
 1. Search show by name and get id of this show
 2. Initialize list of with id's of all episodes
 3. Use check 'manage.CheckEpisode' request to update episodes in loop
+Request:[shows.GetById] - > Request:[manage.CheckEpisode]
 '''
 def markEpisodesAsWatched(nameOfShow):
     global response
     showIds = performedSearch(showName=nameOfShow)
     episodesList = []
     for showId in showIds:
-        requestEpisodesIds  = {
-           "jsonrpc": "2.0",
-            "method": "shows.GetById",
-            "params": {
+        parameters = {
             "showId": showId,
             "withEpisodes": True
-            },
-            "id": 1
         }
+        requestEpisodesIds = prepareJson("shows.GetById", parameters)
         response = simplejson.load(performRequest(requestEpisodesIds, standartHeader))
     for data in response["result"]["episodes"]:
         episodesList.append(data["id"])
     for episodes in episodesList:
-        requestJson  = {
-            "jsonrpc": "2.0",
-            "method": "manage.CheckEpisode",
-            "params": {
+        parameters = {
             "id": episodes,
-             "rating": 0
-            },
-            "id": 1
+            "rating": 0
         }
+        requestJson  = prepareJson("manage.CheckEpisode", parameters)
         simplejson.load(performRequest(requestJson, standartHeader))
 
 #Here you could call functions and test whenever you want
+
+
